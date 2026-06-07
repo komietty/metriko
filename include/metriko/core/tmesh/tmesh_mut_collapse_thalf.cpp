df22d0c03b1997ea3866c0e40f5f5c9ea79d84f3
sakikomikado <komietty@gmail.com>
2026-06-07 20:48:33 +0900

    add tmesh_mut_collapse_range

--- include/metriko/core/tmesh/tmesh_mut_collapse_thalf.cpp
+++ include/metriko/core/tmesh/tmesh_mut_collapse_thalf.cpp
@@ -27,48 +27,41 @@ bool TmeshMut::collapse_thalf(int thid) {
     assert(!(it_crr->side == it_prv->side && it_crr->side == it_nxt->side));
 
     bool collapse_to_prev = it_crr->side != it_prv->side;
-    bool collapse_to_next = !collapse_to_prev;
 
     auto [p_fr, p_to] = [&]{
-      if (collapse_to_prev &&  th_prv.cano) return std::pair{&th_prv.loc_fr(), &th_crr.loc_to()};
-      if (collapse_to_prev && !th_prv.cano) return std::pair{&th_crr.loc_to(), &th_prv.loc_fr()};
-      if (collapse_to_next &&  th_nxt.cano) return std::pair{&th_crr.loc_fr(), &th_nxt.loc_to()};
-      if (collapse_to_next && !th_nxt.cano) return std::pair{&th_nxt.loc_to(), &th_crr.loc_fr()};
+      if ( collapse_to_prev &&  th_prv.cano) return std::pair{&th_prv.loc_fr(), &th_crr.loc_to()};
+      if ( collapse_to_prev && !th_prv.cano) return std::pair{&th_crr.loc_to(), &th_prv.loc_fr()};
+      if (!collapse_to_prev &&  th_nxt.cano) return std::pair{&th_crr.loc_fr(), &th_nxt.loc_to()};
+      if (!collapse_to_prev && !th_nxt.cano) return std::pair{&th_nxt.loc_to(), &th_crr.loc_fr()};
       throw std::runtime_error("no impl");
     }();
 
     auto path = approx_shortest_path(10, hm, *p_fr, *p_to, region);
 
-    vec<int> nids = {};
-    for (const HmLoc& l : path) {
-        tnodes.push_back(l);               // todo: remove duplication!
-        nids.push_back(tnodes.size() - 1); // todo: if exist, push that index
+    // fr/to idx already exists, otherwise all inter-points are newly added. follows this condition.
+    vec<int> nids;
+    int fr_idx = p_fr - tnodes.data();
+    int to_idx = p_to - tnodes.data();
+    for (int i = 0; i < path.size(); ++i) {
+        if      (i == 0)                  nids.push_back(fr_idx);
+        else if (i + 1 == path.size())    nids.push_back(to_idx);
+        else { tnodes.push_back(path[i]); nids.push_back((int)tnodes.size() - 1); }
     }
+
+    auto  it_twn_adj = collapse_to_prev ? circular_next(tq_twn.data, it_twn) : circular_prev(tq_twn.data, it_twn);
+    auto& th_twn_adj = thalfs[it_twn_adj->thid];
+    auto& te_twn_adj = tedges[th_twn_adj.teid];
+    assert(it_twn_adj->side == it_twn->side);
 
     // 1: remove the data from tquads which have collapsed thalfs
     tq_crr.data.erase(it_crr);
     tq_twn.data.erase(it_twn);
 
-    if (collapse_to_prev) {
-        te_prv.nids = nids; // replace prev tedge data
-        auto& th_twn_nxt = thalfs[th_twn.nxid];
-        auto& te_twn_nxt = tedges[th_twn_nxt.teid];
-        auto it_twn_nxt = rg::find(tq_twn.data, th_twn_nxt.id, &TdataMut::thid);
-        assert(it_twn_nxt != tq_twn.data.end());  // mut found in tq_twin
-        assert(it_twn_nxt->side == it_twn->side); // not changing side
-        if (th_crr.cano) te_twn_nxt.insert_locs_after(te_crr.nids);
-        else             te_twn_nxt.insert_locs_front(te_crr.nids);
-    } else {
-        te_nxt.nids = nids; // replace next tedge data
-        auto& th_twn_prv = thalfs[th_twn.pvid];
-        auto& te_twn_prv = tedges[th_twn_prv.teid];
-        auto  it_twn_prv = rg::find(tq_twn.data, th_twn_prv.id, &TdataMut::thid);
-        assert(it_twn_prv != tq_twn.data.end());  // mut found in tq_twin
-        assert(it_twn_prv->side == it_twn->side); // not changing side
-        if (th_crr.cano) te_twn_prv.insert_locs_front(te_crr.nids);
-        else             te_twn_prv.insert_locs_after(te_crr.nids);
+    // 2: collapse to the prv/nxt edge
+    // 3: insert missing segments to the adjacent edge
+    if (collapse_to_prev) { te_prv.nids = nids; if (th_crr.cano) te_twn_adj.insert_locs_after(te_crr.nids); else te_twn_adj.insert_locs_front(te_crr.nids); }
+    else                  { te_nxt.nids = nids; if (th_crr.cano) te_twn_adj.insert_locs_front(te_crr.nids); else te_twn_adj.insert_locs_after(te_crr.nids); }
 
-    }
     return true;
 }
 }
