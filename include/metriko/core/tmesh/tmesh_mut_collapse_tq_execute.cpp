bcac7f3bb7a55ebf7421a5a27f1109ab21bbbfb2
sakikomikado <komietty@gmail.com>
2026-06-24 01:42:44 +0900

    update simple chain tquads collapse

--- include/metriko/core/tmesh/tmesh_mut_collapse_tq_execute.cpp
+++ include/metriko/core/tmesh/tmesh_mut_collapse_tq_execute.cpp
@@ -34,6 +34,24 @@ void TmeshMut::collapse_tquad_chain_execute(Tqchain& chain) {
         return std::nullopt;
     };
 
+    auto thids_of = [&](const HmLoc& fr, const HmLoc& to) -> std::optional<vec<int>> {
+        for (const HmLoc& a : { fr, to }) {
+            const HmLoc& b = a == fr ? to : fr;
+            for (const auto* side : { &chain.thids_t, &chain.thids_b }) {
+                vec<int> res;
+                HmLoc cur = a;
+                while (cur != b) {
+                    auto it = rg::find_if(*side, [&](int t) { return thalfs[t].loc_fr() == cur; });
+                    if (it == side->end()) break;        // cannot continue on this side
+                    res.push_back(*it);
+                    cur = thalfs[*it].loc_to();          // step loc_fr -> loc_to
+                }
+                if (cur == b) { if (a != fr) rg::reverse(res); return res; }  // normalize to fr -> to order
+            }
+        }
+        return std::nullopt;
+    };
+
     for (int i = 0; i < chain.pts.size() - 1; ++i) {
         auto& [l0, v0, adj0, s0] = chain.pts[i];
         auto& [l1, v1, adj1, s1] = chain.pts[i + 1];
@@ -105,24 +123,33 @@ void TmeshMut::collapse_tquad_chain_execute(Tqchain& chain) {
     }
 
     std::cout << "thids_bgn size: " << thids_bgn.size() << std::endl;
-    for (auto& c: chains) {
-        std::cout << "chain size: " << c.size() << std::endl;
-    }
+    for (auto& c: chains) { std::cout << "chain size: " << c.size() << std::endl; }
     std::cout << "thids_end size: " << thids_end.size() << std::endl;
 
     // todo: need to replace some thdis to other thids (multi to multi)
 
-    auto replace = [&](int thid_replace, const vec<int>& chain) {
-        auto& [id, data] = tquads[thalfs[thid_replace].tqid];
-        auto it = rg::find(data, thid_replace, &TdataMut::thid); assert(it != data.end());
-        auto si = it->side;
-        it = data.erase(it);
-        vec<TdataMut> repl;
-        for (int thid : chain) { thalfs[thid].tqid = id; repl.push_back({ thid, si }); }
-        data.insert(it, repl.begin(), repl.end());
+    auto replace = [&](const vec<int>& thids_replace, const vec<int>& chain) {
+        size_t ci = 0;
+        for (int old : thids_replace) {
+            // take the chain slice covering old's span (aligned by the node shared with chain's current pos)
+            const HmLoc& near = thalfs[chain[ci]].loc_fr();
+            const HmLoc& b    = thalfs[old].loc_fr() == near ? thalfs[old].loc_to() : thalfs[old].loc_fr();
+            vec<int> seg;
+            do { seg.push_back(chain[ci]); }
+            while (ci + 1 < chain.size() && thalfs[chain[ci++]].loc_to() != b);
+
+            // same body as single-tquad replace, per element
+            auto& [id, data] = tquads[thalfs[old].tqid];
+            auto it = rg::find(data, old, &TdataMut::thid); assert(it != data.end());
+            auto si = it->side;
+            it = data.erase(it);
+            vec<TdataMut> repl;
+            for (int t : seg) { thalfs[t].tqid = id; repl.push_back({ t, si }); }
+            data.insert(it, repl.begin(), repl.end());
+        }
     };
 
-    auto extend = [&](const ThalfMut& th, bool ahd, int count_fr, int count_to) -> int {
+    auto extend = [&](const ThalfMut& th, bool ahd, int count_fr, int count_to) {
         auto step = [&](int thid) { return ahd ? step_next(thid) : step_prev(thid); };
         auto& th1 = thalfs[step(th.id)];    // nxt or prv
         auto& th2 = thalfs[step(th1.twid)]; // ahd or bhd
@@ -145,7 +172,6 @@ void TmeshMut::collapse_tquad_chain_execute(Tqchain& chain) {
             else if (th.end)     { if (th2.loc_to() == th.loc_to())     th2.end = true; else th2_twn.end = true; }
             else if (th_twn.end) { if (th2.loc_to() == th_twn.loc_to()) th2.end = true; else th2_twn.end = true; }
         }
-        return th1.twid;
     };
 
     { // leftmost
@@ -153,22 +179,38 @@ void TmeshMut::collapse_tquad_chain_execute(Tqchain& chain) {
         auto cfr = count_adj_tquads(th.id);
         auto cto = count_adj_tquads(th.twid);
         bool ahd = th.loc_fr() == l_bgn;
-        int i = extend(th, ahd, cfr, cto);
-        replace(i, thids_bgn);
+        extend(th, ahd, cfr, cto);
+
+        auto l0 = th.loc_to();
+        auto l1 = thalfs[thids_bgn.back()].loc_to();
+        auto op = thids_of(l0, l1).value()
+            | vw::transform([&](int t) { return thalfs[t].twid; })
+            | rg::to<vec<int>>();
+        replace(op, thids_bgn);
     }
     { // rightmost
-        const auto& th_r = thalfs[chain.thid_r];
-        auto cfr = count_adj_tquads(th_r.id);
-        auto cto = count_adj_tquads(th_r.twid);
-        bool ahd = th_r.loc_fr() == l_end;
-        int i = extend(th_r, ahd, cfr, cto);
-        replace(i, thids_end);
+        const auto& th = thalfs[chain.thid_r];
+        auto cfr = count_adj_tquads(th.id);
+        auto cto = count_adj_tquads(th.twid);
+        bool ahd = th.loc_fr() == l_end;
+        extend(th, ahd, cfr, cto);
+
+        auto l0 = th.loc_to();
+        auto l1 = thalfs[thids_end.back()].loc_to();
+        auto op = thids_of(l0, l1).value()
+            | vw::transform([&](int t) { return thalfs[t].twid; })
+            | rg::to<vec<int>>();
+        replace(op, thids_end);
     }
 
     for (auto& c: chains) {
         assert(c.size() >= 2);
-        auto o = thid_of(thalfs[c.front()].loc_fr(), thalfs[c.back()].loc_to());
-        if (o.has_value()) replace(thalfs[o.value()].twid, c);
+        //auto o = thid_of(thalfs[c.front()].loc_fr(), thalfs[c.back()].loc_to());
+        auto o = thids_of(thalfs[c.front()].loc_fr(), thalfs[c.back()].loc_to());
+        auto o2 = o.value()                                        // twins (surviving side), kept in fr->to order
+            | vw::transform([&](int t) { return thalfs[t].twid; })
+            | rg::to<vec<int>>();
+        replace(o2, c);
     }
 
     for (int tqid: chain.tqids) {
